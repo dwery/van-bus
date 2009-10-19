8ba5fff67481a4b46aadc3b8a2c9a41480aae4fd
azummo-google@towertech.it <azummo-google@towertech.it@3d824580-ba7b-11de-bda7-396a5af1c03d>
2009-10-19 23:43:13 +0000

    Removed pengind news from the author

--- trunk/windows-parsing-software/VirtualDash_Src/Virtual_Dash/stdafx.cpp
+++
@@ -1,7 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// Virtual_Dash.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
